4389769ea34d9e07d67df4b079252a01c1c28bbc
Alexandre Brigolin <alexandrebrigolin@hotmail.com>
2023-05-05 10:04:04 -0300

    validando chave novamente

--- CHATGPT /CHATGPT/Service/API.swift
+++ CHATGPT /CHATGPT/Service/API.swift
@@ -8,5 +8,5 @@
 import Foundation
 
 struct API {
-    static let authToken: String = "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"
+    static let authToken: String = "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"
 }
